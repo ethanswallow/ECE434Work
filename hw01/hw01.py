eccad46ba2b106c385d1b5af51a47326e429fc97
ethanswallow <66641400+ethanswallow@users.noreply.github.com>
2022-12-13 16:19:56 -0500

    Update hw01.py

--- hw01/hw01.py
+++ hw01/hw01.py
@@ -1,91 +1,94 @@
-# Ethan Swallow 12/8/2023
+#!/usr/bin/python3
 
-#!/usr/bin/env python3
+#Setup pygame
+import sys, pygame
+pygame.init()
 
-# Simple command line etch-a-sketch framework.
+#Print instructions
+print("Etch-A-Sketch")
+print("Move by entering Commands in command line")
 
-vars util = Required('util')
+#Prompt for row and column numbers
+rows = int(input("Enter rows: "))
+columns = int(input("Enter columns: "))
 
+#Initialize width and height to be about 500
+#Should be exactly divisible by the numer of rows or columns 
+width, height = (int(500/columns)*columns, int(500/rows)*rows)
+white = (255,255,255)
+black = (0,0,0)
 
-xMax = 8
-yMax = 8
-var grid = new Array(yMax);
-var x=0,	// Current position
-    y=0
+#Initialize the screen as a pygame surface
+screen = pygame.display.set_mode((width, height))
 
-#  Initialize th grid to all blanks
-for(var i=0; i<grid.length; i++) {
-    grid[i] = new Array(xMax);
-    for(var j=0; j<grid[i].length; j++) {
-	grid[i][j] = ' ';
-    }
-}
+#Set cursor
+cursorleft = 0
+cursortop = 0
 
-#  Show a + at the current location
-grid[y][x] = '+';
+#Function to move the cursor up
+def moveup():
+	global cursortop,currRow,index
+	#If moving will put it outside of the screen, don't do it
+	if (cursortop - height/rows) >= 0:
+		#Move the cursor
+		cursortop = cursortop - height/rows
 
-#  Print the grid
-def printGrid(grid) {
-  process.stdout.write('   0 1 2 3 4 5 6 7\n');
-  for(var i=0; i<grid.length; i++) {
-    process.stdout.write(util.format("%d: ",i));
-    for(var j=0; j<grid[i].length; j++) {
-      process.stdout.write(util.format("%s ", grid[i][j]));
-    }
-  process.stdout.write("\n");
-  }
-}
+#Function to move the cursor down
+def movedown():
+	global cursortop,currRow,index
+#If moving will put it outside of the screen, don't do it
+	if (cursortop + 2 * height/rows) <= width:
+		#Move the cursor
+		cursortop = cursortop + height/rows
 
-printGrid(grid);
+#Function to move the cursor right
+def moveright():
+	global cursorleft,currColumn,index
+	#If moving will put it outside of the screen, don't do it
+	if (cursorleft + 2 * width/columns) <= width:
+		#Move the cursor
+		cursorleft = cursorleft + width/columns
 
-# This set up the handlers to read user input.
-var readline = require('readline'),
-    rl = readline.createInterface(process.stdin, process.stdout);
+#Function to move the cursor left
+def moveleft():
+	global cursorleft,currColumn,index
+	#If moving will put it outside of the screen, don't do it
+	if (cursorleft - width/columns) >= 0:
+		#Move the cursor
+		cursorleft = cursorleft - width/columns
 
-rl.setPrompt('Direction> ');
-rl.prompt();
+#Function to clear the screen
+def clear():
+	screen.fill(white)
 
-# This function is called whenever a newline is entered
-rl.on('line', function(line) {
-  grid[y][x] = '*';
-  switch(line.trim()) {
-    case 'u':
-      console.log('Up!');
-      y--;
-      if(y<0) {y=grid.length-1;}
-      break;
-    case 'd':
-      console.log('Down!');
-      y++;
-      if(y >= grid.length) {y=0;}
-      break;
-    case 'l':
-      console.log('Left!');
-      x--;
-      if(x<0) {x = grid[y].length-1;}
-      break;
-    case 'r':
-      console.log('Right!');
-      x++;
-      if(x >= grid[y].length) {x=0;}
-      break;
-    case 'c':
-      console.log('Clear!');
-      for(var i=0; i<grid.length; i++) {
-        grid[i] = new Array(xMax);
-        for(var j=0; j<grid[i].length; j++) {
-           grid[i][j] = ' ';
-          }
-      }
-      break;
-    default:
-      console.log('Say what? I might have heard `' + line.trim() + '`');
-      break;
-  }
-  grid[y][x] = '+';
-  printGrid(grid);
-  rl.prompt();
-}).on('close', function() {
-  console.log('Have a great day!');
-  process.exit(0);
-});
+#Initially clear the screen
+screen.fill(white)
+
+#Main loop
+while 1:
+	#Handle events
+	line = input("Enter Command: ")
+	#Handle quit
+	if line =='e':
+		sys.exit()
+	#Handle UP arrow
+	elif line =='u':
+		moveup()
+	#HAndle DOWN arrow
+	elif line == 'd':
+		movedown()
+	#Handle right arrow
+	elif line == 'r':
+		moveright()
+	#Handle left arrow
+	elif line == 'l':
+		moveleft()
+	#Handle SPACEBAR
+	elif line == 'c':
+		clear()
+
+	#Draw the rectangle based on where the cursor is
+	pygame.draw.rect(screen,black,(cursorleft,cursortop,width/columns,height/rows))
+
+	#Update the screen
+	pygame.display.update()
