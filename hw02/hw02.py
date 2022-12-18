3a1c4ab82ab018cdc2a0ab84864d33c08c553c88
ethanswallow <66641400+ethanswallow@users.noreply.github.com>
2022-12-18 15:15:07 -0500

    Update hw02.py

--- hw02/hw02.py
+++ hw02/hw02.py
@@ -5,9 +5,8 @@ import Adafruit_BBIO.GPIO as GPIO
 import time
 
 #Setup pygame
-import sys, pygame
-pygame.init()
-pygame.event.set_allowed([pygame.QUIT, pygame.KEYDOWN])
+import sys
+import numpy as np
 
 #Set up GPIO
 buttonUp = "P9_21"
@@ -30,60 +29,45 @@ print("Move and clear with buttons")
 rows = int(input("Enter rows: "))
 columns = int(input("Enter columns: "))
 
-#Initialize width and height to be about 500
-#Should be exactly divisible by the number of rows or columns
-size = width, height = int(500/columns)*columns, int(500/rows)*rows
-white = (255,255,255)
-black = (0,0,0)
+xMax = rows
+yMax = columns
+x = 0
+y = 0
+grid = np.full((xMax,yMax),' ')
 
-#Initialize the screen as a pygame surface
-screen = pygame.display.set_mode(size)
-
-#Set cursor
-cursorleft = 0
-cursortop = 0
+def printGrid(grid):
+    grid[x,y] = '+'
+    print(grid)
 
 #Function to move the cursor and clear the screen
 def move(channel):
-	global cursortop, cursorleft, currRow, index, buttonUp, buttonDown, buttonRight, buttonLeft, buttonClear
+	global x, y, yMax,xMax, grid, buttonUp, buttonDown, buttonRight, buttonLeft, buttonClear
 	#if statement to choose which button was pressed
 	if channel == buttonUp:
+		y -= 1
 		print("Up")
-		#If moving will put it outside of the screen, don't do it
-		if (cursortop - height/rows) >= 0:
-			#Move the cursor
-			cursortop = cursortop - height/rows
-			#Draw the rectangle based on where the cursor is
-			pygame.draw.rect(screen,black,(cursorleft,cursortop,width/columns,height/rows))
-			
+		if y<0:
+				y= yMax -1
 	elif channel == buttonDown:
 		print("Down")
-		#If moving will put it outside of the screen, don't do it
-		if (cursortop + 2 * height/rows) <= width:
-			#Move the cursor
-			cursortop = cursortop + height/rows
-			
+		y += 1
+		if y >= yMax:
+				y=0
 	elif channel == buttonRight:
 		print("Right")
-		#If moving will put it outside of the screen, don't do it
-		if (cursorleft + 2 * width/columns) <= width:
-			#Move the cursor
-			cursorleft = cursorleft + width/columns
-	
+		x += 1
+		if x >= xMax:
+				x=0
 	elif channel == buttonLeft:
-		print("Left")
-		#If moving will put it outside of the screen, don't do it
-		if (cursorleft - width/columns) >= 0:
-			#Move the cursor
-			cursorleft = cursorleft - width/columns
-			
+		x -= 1
+		print('Left!')
+		if x<0:
+			x = xMax -1
 	elif channel == buttonClear:
 		print("Clear")
-		screen.fill(white)
+		grid = np.full((xMax,yMax),' ')
 
-	#Draw the rectangle based on where the cursor is
-	pygame.draw.rect(screen,black,(cursorleft,cursortop,width/columns,height/rows))
-
+	printGrid(grid)
 
 #Add event Listeners
 GPIO.add_event_detect(buttonUp, GPIO.FALLING, callback=move)
@@ -92,20 +76,7 @@ GPIO.add_event_detect(buttonRight, GPIO.FALLING, callback=move)
 GPIO.add_event_detect(buttonLeft, GPIO.FALLING, callback=move)
 GPIO.add_event_detect(buttonClear, GPIO.FALLING, callback=move)
 
-#Initially clear the screen
-screen.fill(white)
-
-# Draw the rectangle initially
-pygame.draw.rect(screen,black,(cursorleft,cursortop,width/columns,height/rows))
-
-#Main loop
 while 1:
-	#Handle events
-	for event in pygame.event.get():
-		#Handle quit
-		if event.type == pygame.QUIT:
-			sys.exit()
-			GPIO.cleanup()
-
-	#Update the screen
-	pygame.display.update()
+	line = input("Enter Command: ")
+	if line == 'e':
+		sys.exit()
