a4f7b65429953a50203d0eca68338cff2bbc15d0
ethanswallow <66641400+ethanswallow@users.noreply.github.com>
2023-01-09 15:52:13 -0500

    Create text.sh

---
+++ hw04/text.sh
@@ -0,0 +1,15 @@
+# Here's how to use imagemagick to display text
+# Make a blank image
+SIZE=320x240
+TMP_FILE=/tmp/frame.png
+
+# From: http://www.imagemagick.org/Usage/text/
+# Place boris, but add text above 
+convert boris2.png -pointsize 24 \
+      label:'Ethan Swallow' +swap -append \
+      $TMP_FILE
+
+#Display the image
+sudo fbi -noverbose -T 1 $TMP_FILE
+
+# convert -list font
